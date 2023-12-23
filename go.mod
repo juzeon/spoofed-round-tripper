bd35adcd71a30e7832d94bb82a8d1e2a3ad896a0
juzeon <812312770@qq.com>
2023-12-23 21:39:10 +0800

    init round tripper

---
+++ go.mod
@@ -0,0 +1,21 @@
+module github.com/juzeon/spoof-round-tripper
+
+go 1.21
+
+require (
+	github.com/bogdanfinn/fhttp v0.5.27
+	github.com/bogdanfinn/tls-client v1.7.1
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.6 // indirect
+	github.com/bogdanfinn/utls v1.6.1 // indirect
+	github.com/cloudflare/circl v1.3.6 // indirect
+	github.com/klauspost/compress v1.17.4 // indirect
+	github.com/quic-go/quic-go v0.40.1 // indirect
+	github.com/tam7t/hpkp v0.0.0-20160821193359-2b70b4024ed5 // indirect
+	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/net v0.19.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
---
+++ round_tripper.go
@@ -0,0 +1,55 @@
+package spoof_round_tripper
+
+import (
+	"fmt"
+	fhttp "github.com/bogdanfinn/fhttp"
+	tlsclient "github.com/bogdanfinn/tls-client"
+	"net/http"
+)
+
+type SpoofRoundTripper struct {
+	Client tlsclient.HttpClient
+}
+
+func NewSpoofRoundTripper(httpClientOption ...tlsclient.HttpClientOption) (*SpoofRoundTripper, error) {
+	c, err := tlsclient.NewHttpClient(tlsclient.NewNoopLogger(), httpClientOption...)
+	if err != nil {
+		return nil, fmt.Errorf("error creating client: %w", err)
+	}
+	return &SpoofRoundTripper{
+		Client: c,
+	}, nil
+}
+
+func (s SpoofRoundTripper) RoundTrip(hReq *http.Request) (*http.Response, error) {
+	fReq, err := fhttp.NewRequest(hReq.Method, hReq.URL.String(), hReq.Body)
+	if err != nil {
+		return nil, err
+	}
+	fReq.Header = fhttp.Header(hReq.Header)
+	fReq.Trailer = fhttp.Header(hReq.Trailer)
+	fReq.Form = hReq.Form
+	fReq.MultipartForm = hReq.MultipartForm
+	fReq.PostForm = hReq.PostForm
+
+	fResp, err := s.Client.Do(fReq)
+	if err != nil {
+		return nil, fmt.Errorf("error fetching response: %w", err)
+	}
+	return &http.Response{
+		Status:           fResp.Status,
+		StatusCode:       fResp.StatusCode,
+		Proto:            fResp.Proto,
+		ProtoMajor:       fResp.ProtoMajor,
+		ProtoMinor:       fResp.ProtoMinor,
+		Header:           http.Header(fResp.Header),
+		Body:             fResp.Body,
+		ContentLength:    fResp.ContentLength,
+		TransferEncoding: fResp.TransferEncoding,
+		Close:            fResp.Close,
+		Uncompressed:     fResp.Uncompressed,
+		Trailer:          http.Header(fResp.Trailer),
+		Request:          hReq,
+		TLS:              nil,
+	}, nil
+}
